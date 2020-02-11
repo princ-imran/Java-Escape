0bbe1ac6b88bb84040b1ce3bfe30ed09f09e8fbf
Imran Hoshain <princ.imran@gmail.com>
2020-02-12 00:54:13 +0600

    Java Escape

---
+++ src/javaescape/JavaEscape.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaescape;
+
+/**
+ *
+ * @author Spy Hacker
+ */
+public class JavaEscape {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Name: Imran \b Hoshain");//Use for backspace
+        System.out.print("\nAge: 26");//Use for new line
+        System.out.println("Semister: 3rd \tUniversity: Canadian University of Bangladesh");//Use for single tab or step
+        System.out.println("Imran \r Hoshain"); //Use for Carriage Return
+        System.out.println("\"Imran Hoshain\""); //Use for ""
+        System.out.println("\\Imran Hoshain\'"); //Use \ and '
+        
+    }
+    
+}
